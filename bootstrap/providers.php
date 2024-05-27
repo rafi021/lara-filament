2903204d4d152b465c63f8e4c61c48b7f1a68868
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-05-27 11:28:29 +0600

    filament install

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Providers\Filament\AdminPanelProvider;
+
+return [
+    AdminPanelProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+];
